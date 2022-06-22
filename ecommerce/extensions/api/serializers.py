ab67f04b98cd86f842270e80ef30ed624dd9586a
julianajlk <julianajlk@gmail.com>
2022-06-22 16:29:26 -0400

    feat: Modify discount to include offer_type in Orders API

--- ecommerce/extensions/api/serializers.py
+++ ecommerce/extensions/api/serializers.py
@@ -412,10 +412,18 @@ class OrderSerializer(serializers.ModelSerializer):
 
     def get_discount(self, obj):
         try:
-            discount = obj.discounts.all()[0]
-            return str(discount.amount)
-        except IndexError:
-            return '0'
+            discount = obj.discounts.first()
+            if discount:
+                return {
+                    'offer_type': discount.offer.offer_type,
+                    'amount': str(discount.amount),
+                }
+        except (AttributeError, TypeError, ValueError):
+            logger.exception(
+                'Failed to retrieve get_discount for order: [%s]',
+                obj
+            )
+        return None
 
     def get_enable_hoist_order_history(self, obj):
         try:
