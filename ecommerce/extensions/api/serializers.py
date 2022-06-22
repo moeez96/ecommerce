c1bfb8ab8ce6bb8edc2ed01b4742011eca4771a3
julianajlk <julianajlk@gmail.com>
2022-06-22 19:10:40 -0400

    feat: Add total_before_discounts_incl_tax to Orders API serializer

--- ecommerce/extensions/api/serializers.py
+++ ecommerce/extensions/api/serializers.py
@@ -394,6 +394,7 @@ class OrderSerializer(serializers.ModelSerializer):
     enable_hoist_order_history = serializers.SerializerMethodField()
     payment_method = serializers.SerializerMethodField()
     enterprise_customer_info = serializers.SerializerMethodField()
+    total_before_discounts_incl_tax = serializers.SerializerMethodField()
 
     def get_vouchers(self, obj):
         try:
@@ -465,6 +466,12 @@ class OrderSerializer(serializers.ModelSerializer):
             )
         return None
 
+    def get_total_before_discounts_incl_tax(self, obj):
+        try:
+            return str(obj.total_before_discounts_incl_tax)
+        except ValueError:
+            return None
+
     class Meta:
         model = Order
         fields = (
@@ -479,6 +486,7 @@ class OrderSerializer(serializers.ModelSerializer):
             'payment_processor',
             'payment_method',
             'status',
+            'total_before_discounts_incl_tax',
             'total_excl_tax',
             'user',
             'vouchers',
